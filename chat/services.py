7ab95300648a18d54315a873bf6c016b588523b8
tema1998 <104845356+tema1998@users.noreply.github.com>
2024-02-21 15:06:49 +0300

    Add dockstring to Chat services

--- chat/services.py
+++ chat/services.py
@@ -4,30 +4,51 @@ from .models import Chat, Message
 
 
 def get_chats_list(user):
+    """
+    Return queryset user's chats sorted by last update time.
+    """
     return Chat.objects.prefetch_related('members').filter(members=user).order_by('-last_update')
 
 
 def get_chat(chat_id):
+    """
+    Return chat object.
+    """
     return Chat.objects.get(id=chat_id)
 
 
 def get_chat_members(chat):
+    """
+    Return queryset of chat members.
+    """
     return chat.members.all()
 
 
 def get_chat_messages(chat):
+    """
+    Return chat's messages.
+    """
     return Message.objects.filter(chat=chat).order_by('-date_added')
 
 
 def get_user(user_id):
+    """
+    Return user object.
+    """
     return User.objects.get(id=user_id)
 
 
 def get_chat_with_two_users(first_user_id, second_user_id):
+    """
+    Return chat with users by users ID's.
+    """
     return Chat.objects.filter(members__id=first_user_id).filter(members__id=second_user_id)
 
 
 def create_chat_with_two_users(first_user, second_user):
+    """
+    Create chat with two users and return chat object.
+    """
     chat = Chat.objects.create()
     chat.members.add(first_user, second_user)
     chat.save()
@@ -35,16 +56,28 @@ def create_chat_with_two_users(first_user, second_user):
 
 
 def get_message(message_id):
+    """
+    Return message object by ID.
+    """
     return Message.objects.get(id=message_id)
 
 
 def delete_message(message):
+    """
+    Delete message object.
+    """
     message.delete()
 
 
 def delete_chat(chat):
+    """
+    Delete chat object.
+    """
     chat.delete()
 
 
 def clear_chat(chat):
+    """
+    Clear chat history.
+    """
     chat.messages.all().delete()
