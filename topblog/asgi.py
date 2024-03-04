2e9302822108829e2c5e976c96f1dbdcad801ad4
tema1998 <104845356+tema1998@users.noreply.github.com>
2024-03-04 23:28:36 +0300

    Fix asgi file to deploy

--- topblog/asgi.py
+++ topblog/asgi.py
@@ -1,19 +1,23 @@
 import os
-
+import django
 from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "topblog.settings")
+django.setup()
+django_asgi_app = get_asgi_application()
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-
+from channels.security.websocket import AllowedHostsOriginValidator
+from django.urls import path
+from chat import consumers
 import chat.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'topblog.settings')
-
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            chat.routing.websocket_urlpatterns
+    "http": django_asgi_app,
+    "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(
+                URLRouter(chat.routing.websocket_urlpatterns)
         )
-    )
+    ),
 })
